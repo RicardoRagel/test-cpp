58cfd5b265c16ccf433bcd1e8d67d0b41921b935
RicardoRagel <ricardoragel4@gmail.com>
2022-08-04 18:24:26 +0200

    Added an example about how to use bind to get a callback of a class function member

---
+++ TestFunctionAndBind/src/function_and_bind_in_a_class_test.cpp
@@ -0,0 +1,43 @@
+/*
+
+Testing the C++ std::function<> type and std::bind() function 
+
+*/
+
+#include <iostream> // std::cout
+#include <functional> // std::function
+
+// Aliases
+using namespace std;
+
+using namespace std::placeholders;
+
+
+// A class
+class A
+{
+    public:
+        float method(int16_t a, int16_t b)
+        {
+            return a*b*1.1;
+        }
+};
+
+int main(int argc, char **argv)
+{
+    cout << endl;
+    cout << "(main) Test function and bind in a class:" << endl;
+    cout << "-----------------------------------------" << endl;
+
+    cout << "" << endl;
+
+    // Getting a function pointer to the class method (with the same args, so using _1 and _2)
+    // Take note that it is neccessary to pass the class method reference and the object reference separatelly.
+    A a;
+    std::function<float(int16_t, int16_t)> methodPtr = std::bind(&A::method, &a, _1, _2);
+
+    cout << "Testing member function pointer: " << methodPtr(2,3) << endl;
+
+    // When is it useful? For example, when we have to pass to a library function a callback function that is a member of our class.
+    return 0;
+}
